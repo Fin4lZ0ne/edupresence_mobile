93003a76bf2c823c14e11b8ecd00cc68206aebcd
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2025-05-23 10:18:54 +0700

    add animation second splash screen with gif image

--- lib/ui/widget/launcher.dart
+++ lib/ui/widget/launcher.dart
@@ -14,31 +14,48 @@ class launcher extends StatefulWidget {
 }
 
 class _launcherState extends State<launcher> {
+  bool _showSplash = true;
+
   void cekPilihan() async {
     final prefs = await SharedPreferences.getInstance();
 
-    
-      if (prefs.getBool('login') == true) {
-        Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) {
-          return const HomePage();
-        }));
-      } else {
-        Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) {
-          return const LoginPage();
-        }));
-      }
+    if (prefs.getBool('login') == true) {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+        return const HomePage();
+      }));
+    } else {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+        return const LoginPage();
+      }));
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    cekPilihan();
+
+    Future.delayed(const Duration(milliseconds: 3500), () {
+      setState(() {
+        _showSplash = false;
+      });
+
+      cekPilihan();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    if (_showSplash) {
+      return Scaffold(
+        backgroundColor: Colors.white,
+        body: SizedBox.expand(
+          child: Image.asset(
+            'assets/splash/logo_anim.gif',
+            fit: BoxFit.cover, // Untuk menyesuaikan ke layar penuh
+          ),
+        ),
+      );
+    }
     return Container();
   }
 }
